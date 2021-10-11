0ea592f4b9145372dd4f9cc83de0998e1cff6073
julieviktoria <julieviktoria1@hotmail.com>
2021-10-11 14:59:18 +0200

    :construction: in progress

---
+++ js/script.js
@@ -0,0 +1,32 @@
+/* 
+const products = [
+    {
+        name:
+        id:
+        price:
+        preview:
+    },
+    {
+        name:
+        id:
+        price:
+        preview:
+    },
+    {
+        name:
+        id:
+        price:
+        preview:
+    },
+]
+*/ 
+
+const productDIV = 
+
+
+function clearCart() {
+    const cart = document.querySelector('.cart')
+    cart.innerText = ' '
+}
+
+GamepadButton.addEventListener('click', clearCart)
