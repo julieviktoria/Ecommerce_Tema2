eac928acb4509c00e578716c559956b4c62ffc72
Julie Viktoria <julieviktoria1@hotmail.com>
2021-10-29 16:43:21 +0200

    🎨 products js

--- js/script.js
+++ js/script.js
@@ -1,27 +1,70 @@
-/* 
+
 const products = [
     {
-        name:
-        id:
-        price:
-        preview:
+        name: "Satin Wrap Blouse",
+        id: "1",
+        price: 49,
+        preview: "./assets/product7_1.webp"
     },
     {
-        name:
-        id:
-        price:
-        preview:
+        name: "Split Hem Trousers Vinyl",
+        id: "2",
+        price: 69,
+        preview: "./assets/product5_1.webp"
     },
     {
-        name:
-        id:
-        price:
-        preview:
+        name: "Bodycon Satin Dress",
+        id: "3",
+        price: 99,
+        preview: "./assets/product6_1.webp"
+    },
+    {
+        name: "Poplin Blouse",
+        id: "4",
+        price: 49,
+        preview: "./assets/product1_1.webp"
+    },
+    {
+        name: "Faux Leather Purse",
+        id: "5",
+        price: 39,
+        preview: "./assets/product2_1.webp"
+    },
+    {
+        name: "Mom Jeans Light Blue",
+        id: "6",
+        price: 59,
+        preview: "./assets/product3_1.webp"
     },
 ]
-*/ 
 
-const productDIV = 
+const viewItems = function (itemsArray) {
+    const grid = itemsArray.map((product) => {
+        const productContainer = document.querySelector("#container")
+        const productCard = document.createElement("div")
+        const productDescription = document.createElement("div")
+        const productTitle = document.createElement("div")
+        const productPrice = document.createElement("div")
+        const productImg = document.createElement("img")
+
+        productCard.className = "product"
+        productDescription.className = "description"
+        productTitle.className = "product-title"
+        productPrice.className = "price"
+
+        productTitle.innerHTML = product.name
+        productPrice.innerHTML = `$${product.price}`
+        productImg.src = product.preview
+
+        productContainer.appendChild(productCard)
+        productCard.appendChild(productDescription)
+        productDescription.appendChild(productTitle)
+        productDescription.appendChild(productPrice)
+        productCard.appendChild(productImg)
+    })
+}
+
+/* const productDIV = 
 
 
 function clearCart() {
@@ -29,4 +72,8 @@ function clearCart() {
     cart.innerText = ' '
 }
 
-GamepadButton.addEventListener('click', clearCart)
+GamepadButton.addEventListener('click', clearCart) */
+
+viewItems(products)
+
+console.log(products)
